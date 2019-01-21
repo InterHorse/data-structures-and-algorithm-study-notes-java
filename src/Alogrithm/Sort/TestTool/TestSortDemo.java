4bd51b54a3602a2564ebc5b1b6b2f1f050c27a92
Martin Ma <mayuzhe1222@qq.com>
2019-01-21 10:56:03 +0800

    [feat] 添加快速排序 / 快速排序随机优化及测试类

--- src/Alogrithm/Sort/TestTool/TestSortDemo.java
+++ src/Alogrithm/Sort/TestTool/TestSortDemo.java
@@ -3,6 +3,7 @@ package Alogrithm.Sort.TestTool;
 import Alogrithm.Sort.HeapSort.HeapSort;
 import Alogrithm.Sort.InsertionSort.InsertionSort;
 import Alogrithm.Sort.MergeSort.MergeSort;
+import Alogrithm.Sort.QuickSort.QuickSort;
 import Alogrithm.Sort.ShellSort.ShellSort;
 
 /**
@@ -11,14 +12,16 @@ import Alogrithm.Sort.ShellSort.ShellSort;
  */
 public class TestSortDemo {
     public static void main(String[] args) {
-        Integer[] integers1 = IntegerTestTool.getRandomArr(0, 10000000, 100000);
+        Integer[] integers1 = IntegerTestTool.getRandomArr(0, 100000000, 1000000);
         Integer[] integers2 = IntegerTestTool.copyArr(integers1);
         Integer[] integers3 = IntegerTestTool.copyArr(integers1);
         Integer[] integers4 = IntegerTestTool.copyArr(integers1);
+        Integer[] integers5 = IntegerTestTool.copyArr(integers1);
 
         TestSort.testSort("HeapSort", integers1, new HeapSort());
-        TestSort.testSort("InsertionSort", integers2, new InsertionSort());
+//        TestSort.testSort("InsertionSort", integers2, new InsertionSort());
         TestSort.testSort("ShellSort", integers3, new ShellSort());
         TestSort.testSort("MergeSort", integers4, new MergeSort());
+        TestSort.testSort("QuickSort", integers5, new QuickSort());
     }
 }
