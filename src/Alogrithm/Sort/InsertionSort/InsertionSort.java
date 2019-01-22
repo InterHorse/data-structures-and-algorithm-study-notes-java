639ac1b4d54058f8ba6b3bb3eed77ea992be6254
Martin Ma <mayuzhe1222@qq.com>
2019-01-22 10:24:24 +0800

    [feat] 添加双路快排和三路快排

--- src/Alogrithm/Sort/InsertionSort/InsertionSort.java
+++ src/Alogrithm/Sort/InsertionSort/InsertionSort.java
@@ -32,6 +32,26 @@ public class InsertionSort extends BaseSort {
         }
     }
 
+    /**
+     * InsertionSort 插入排序
+     *
+     * @param arr 被排序的数组
+     * @param <T>
+     */
+    public static <T extends Comparable<? super T>> void insertionSort(T[] arr, int l, int r) {
+        int p;
+        for (int i = l + 1; i <= r; i++) {
+            // 储存当前访问的节点值
+            T tmp = arr[i];
+            for (p = i; p > l && tmp.compareTo(arr[p - 1]) < 0; p--) {
+                // 如果tmp小于arr[p - 1]的值，则后移一位
+                arr[p] = arr[p - 1];
+            }
+            // 将tmp替换p位置的值（上面的for已经将原来的p后移了）
+            arr[p] = tmp;
+        }
+    }
+
     @Override
     public <T extends Comparable<? super T>> void sort(T[] array) {
         insertionSort(array);
